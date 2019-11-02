952e4b541e786e08d01c03ce81e58e203703151a
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-11-01 20:30:28 -0700

    added functionality for food2, 3, 4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -39,6 +39,9 @@ $(document).ready(function () {
             goalNumber = Math.floor(Math.random() * 101 + 19);
             $("#goalNumberShow").text(goalNumber);
             foodvalue1 = Math.floor(Math.random() * 11 + 1);
+            foodvalue2 = Math.floor(Math.random() * 11 + 1)
+            foodvalue3 = Math.floor(Math.random() * 11 + 1)
+            foodvalue4 = Math.floor(Math.random() * 11 + 1)
             totalWins++;
             $("#winsx").text(totalWins);
             console.log("wins: " + totalWins);
@@ -53,9 +56,169 @@ $(document).ready(function () {
             goalNumber = Math.floor(Math.random() * 101 + 19);
             $("#goalNumberShow").text(goalNumber);
             foodvalue1 = Math.floor(Math.random() * 11 + 1);
+            foodvalue2 = Math.floor(Math.random() * 11 + 1)
+            foodvalue3 = Math.floor(Math.random() * 11 + 1)
+            foodvalue4 = Math.floor(Math.random() * 11 + 1)
             fullness = 0;
             totalLosses++;
             $("#losses").text(totalLosses);
         }
     })
+
+    // end food 1 
+
+    // start food 2 
+
+    // Event for clicking on food 2
+    $("#food2").on("click", function () {
+        fullness = fullness + foodvalue2;
+
+        // Shows fullness level in console 
+        console.log("fullness:" + fullness);
+
+        // Changes instructions display :)
+        $("#instructions").text("Keep Feeding The Cat!!! >:)");
+
+        // Changes current fatness display :)
+        $("#currentfatness").text(fullness);
+
+        // Win condition
+        if (fullness == goalNumber) {
+            $("#instructions").text("The Cat is now perfectly stuffed :)");
+
+            // resets game goal 0, fullness 0,  new goal number, new food values
+            goalNumber = Math.floor(Math.random() * 101 + 19);
+            $("#goalNumberShow").text(goalNumber);
+            foodvalue1 = Math.floor(Math.random() * 11 + 1);
+            foodvalue2 = Math.floor(Math.random() * 11 + 1)
+            foodvalue3 = Math.floor(Math.random() * 11 + 1)
+            foodvalue4 = Math.floor(Math.random() * 11 + 1)
+            totalWins++;
+            $("#winsx").text(totalWins);
+            console.log("wins: " + totalWins);
+            fullness = 0;
+        }
+
+        // Lose condition 
+        else if (fullness > goalNumber) {
+            $("#instructions").text("The cat has exploded!!! :( :( :(");
+
+            // resets game turn into function?
+            goalNumber = Math.floor(Math.random() * 101 + 19);
+            $("#goalNumberShow").text(goalNumber);
+            foodvalue1 = Math.floor(Math.random() * 11 + 1);
+            foodvalue2 = Math.floor(Math.random() * 11 + 1)
+            foodvalue3 = Math.floor(Math.random() * 11 + 1)
+            foodvalue4 = Math.floor(Math.random() * 11 + 1)
+            fullness = 0;
+            totalLosses++;
+            $("#losses").text(totalLosses);
+        }
+    })
+
+    // end food 2 
+
+    // start food 3 
+
+
+    // Event for clicking on food 3
+    $("#food3").on("click", function () {
+        fullness = fullness + foodvalue3;
+
+        // Shows fullness level in console 
+        console.log("fullness:" + fullness);
+
+        // Changes instructions display :)
+        $("#instructions").text("Keep Feeding The Cat!!! >:)");
+
+        // Changes current fatness display :)
+        $("#currentfatness").text(fullness);
+
+        // Win condition
+        if (fullness == goalNumber) {
+            $("#instructions").text("The Cat is now perfectly stuffed :)");
+
+            // resets game goal 0, fullness 0,  new goal number, new food values
+            goalNumber = Math.floor(Math.random() * 101 + 19);
+            $("#goalNumberShow").text(goalNumber);
+            foodvalue1 = Math.floor(Math.random() * 11 + 1);
+            foodvalue2 = Math.floor(Math.random() * 11 + 1)
+            foodvalue3 = Math.floor(Math.random() * 11 + 1)
+            foodvalue4 = Math.floor(Math.random() * 11 + 1)
+            totalWins++;
+            $("#winsx").text(totalWins);
+            console.log("wins: " + totalWins);
+            fullness = 0;
+        }
+
+        // Lose condition 
+        else if (fullness > goalNumber) {
+            $("#instructions").text("The cat has exploded!!! :( :( :(");
+
+            // resets game turn into function?
+            goalNumber = Math.floor(Math.random() * 101 + 19);
+            $("#goalNumberShow").text(goalNumber);
+            foodvalue1 = Math.floor(Math.random() * 11 + 1);
+            foodvalue2 = Math.floor(Math.random() * 11 + 1)
+            foodvalue3 = Math.floor(Math.random() * 11 + 1)
+            foodvalue4 = Math.floor(Math.random() * 11 + 1)
+            fullness = 0;
+            totalLosses++;
+            $("#losses").text(totalLosses);
+        }
+    })
+
+    // end food 3 
+
+    // start food 4 
+
+
+    // Event for clicking on food 4
+    $("#food4").on("click", function () {
+        fullness = fullness + foodvalue4;
+
+        // Shows fullness level in console 
+        console.log("fullness:" + fullness);
+
+        // Changes instructions display :)
+        $("#instructions").text("Keep Feeding The Cat!!! >:)");
+
+        // Changes current fatness display :)
+        $("#currentfatness").text(fullness);
+
+        // Win condition
+        if (fullness == goalNumber) {
+            $("#instructions").text("The Cat is now perfectly stuffed :)");
+
+            // resets game goal 0, fullness 0,  new goal number, new food values
+            goalNumber = Math.floor(Math.random() * 101 + 19);
+            $("#goalNumberShow").text(goalNumber);
+            foodvalue1 = Math.floor(Math.random() * 11 + 1);
+            foodvalue2 = Math.floor(Math.random() * 11 + 1)
+            foodvalue3 = Math.floor(Math.random() * 11 + 1)
+            foodvalue4 = Math.floor(Math.random() * 11 + 1)
+            totalWins++;
+            $("#winsx").text(totalWins);
+            console.log("wins: " + totalWins);
+            fullness = 0;
+        }
+
+        // Lose condition 
+        else if (fullness > goalNumber) {
+            $("#instructions").text("The cat has exploded!!! :( :( :(");
+
+            // resets game turn into function?
+            goalNumber = Math.floor(Math.random() * 101 + 19);
+            $("#goalNumberShow").text(goalNumber);
+            foodvalue1 = Math.floor(Math.random() * 11 + 1);
+            foodvalue2 = Math.floor(Math.random() * 11 + 1)
+            foodvalue3 = Math.floor(Math.random() * 11 + 1)
+            foodvalue4 = Math.floor(Math.random() * 11 + 1)
+            fullness = 0;
+            totalLosses++;
+            $("#losses").text(totalLosses);
+        }
+    })
+
+    // end food 4 
 })
